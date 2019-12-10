1616a1116d07b7f0d20e857ac55e26ed4fa4993f
Theo Carney <theoluyi@gmail.com>
2019-12-10 19:10:12 +0000

    Automatically backed up by Learn

--- lib/nds_extract.rb
+++ lib/nds_extract.rb
@@ -1,6 +1,7 @@
 $LOAD_PATH.unshift(File.dirname(__FILE__))
 require_relative './directors_database'
 
+
 def directors_totals(source)
   result = {}
   director_index = 0
@@ -9,6 +10,7 @@ def directors_totals(source)
     result[director[:name]] = gross_for_director(director)
     director_index += 1
   end
+  # pp result
   result
 end
 
@@ -25,19 +27,48 @@ def gross_for_director(d)
 end
 
 def list_of_directors(source)
-  # Write this implementation
+  array = [] 
+  index = 0 
+  
+  while index < source.length do 
+    array.push(source[index][:name])
+    index += 1 
+  end
+  
+  # Write this implementation 
+  # [{:name=>"Larry"}, {:name=>"Curly"}, {:name=>"Moe"}, {:name=>"Iggy"}] 
+  # rspec spec/nds_extract.rb -e list_of_directors
+  
+  array 
 end
 
 def total_gross(source)
+  total = 0 
+  index = 0 
+  
+  # puts list_of_directors[index]
+  new = directors_totals(source)
+  pp new
+  
+  while index < list_of_directors.length do 
+    total += directors_totals[list_of_directors[index]]
+    index += 1 
+  end 
+  
+  
+  # directors_totals[]... 
+
   # Write this implementation
   #
   # Should use methods:
-  # 1. directors_totals: returns a Hash of { dir_name => gross }
+  # 1. directors_totals: returns a Hash of 
+  # { dir_name => gross, "Stephen Spielberg"=>1357566430, "Russo Brothers"=>2281002470, "James Cameron"=>2571020373 }
   # 2. list_of_directors: names provides an Array of directors names (use
   #
   # Visit each key (i.e. director name), look up the value in the hash
   # returned by directors_totals, and add it to a running total. When done,
   # return the total
+  total 
 end
 
 
